e8bf014e65cf8bdf74c2383bf802eb675aca2cfa
AnhSon-DHSPDHDN <59197139+AnhSon-DHSPDHDN@users.noreply.github.com>
2021-06-19 13:11:07 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import MainContainer from 'components/MainContainer';
+import MainLayout from 'layouts/MainLayout';
+
 function App() {
-	return <div className='App'>Hello Word!</div>;
+	return (
+		<div className='App'>
+			<MainLayout>
+				<MainContainer />
+			</MainLayout>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './style.scss';
+import Logo from 'assets/imgs/logo-is.webp';
+
+export default function Header() {
+	return (
+		<header id='header'>
+			<div className='header__container'>
+				<a className='logo-is mr-4' href='./'>
+					<img src={Logo} alt='logo' />
+				</a>
+				<a className='logo-name mr-4' href='./'>
+					amazing
+				</a>
+				<div className='input-group'>
+					<div className='input-search'>
+						<input
+							type='text'
+							className='form-control ais-search-box--input'
+							autoCapitalize='off'
+							autoComplete='off'
+							autoCorrect='off'
+							placeholder='Search a product'
+							spellCheck='false'
+						/>
+					</div>
+					<span className='input-group-btn'>
+						<button>
+							<i className='far fa-search fw-6'></i>
+						</button>
+					</span>
+				</div>
+			</div>
+		</header>
+	);
+}
